e8d417a2154cbc5b603538d023336571f2f120c0
VRn00b <vrn00b@gmail.com>
2021-08-09 10:05:04 +0300

    gui rewards fix

--- scam-blockchain-gui/src/util/blockRewards.ts
+++ scam-blockchain-gui/src/util/blockRewards.ts
@@ -25,20 +25,20 @@ export function calculatePoolReward(height: number): Big {
 
 export function calculateBaseFarmerReward(height: number): Big {
   if (height === 0) {
-    return MOJO_PER_SCAM.times(21000000).times(8 / 8);
+    return MOJO_PER_SCAM.times(3000000).times(8 / 8);
   }
   if (height < 3 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_SCAM.times(2).times(8 / 8);
+    return MOJO_PER_SCAM.times(10).times(8 / 8);
   }
   if (height < 6 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_SCAM.times(1).times(8 / 8);
+    return MOJO_PER_SCAM.times(5).times(8 / 8);
   }
   if (height < 9 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_SCAM.times(0.5).times(8 / 8);
+    return MOJO_PER_SCAM.times(2.5).times(8 / 8);
   }
   if (height < 12 * BLOCKS_PER_YEAR) {
-    return MOJO_PER_SCAM.times(0.25).times(8 / 8);
+    return MOJO_PER_SCAM.times(1.25).times(8 / 8);
   }
 
-  return MOJO_PER_SCAM.times(0.125).times(8 / 8);
+  return MOJO_PER_SCAM.times(0.625).times(8 / 8);
 }
